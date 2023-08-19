28f8585aa6a3d4b8277a6f95a25bc10505370062
Nadin21 <nadin21dir@gmail.com>
2023-08-19 16:37:12 +0300

    feat: added converting a two-dimensional array to a one-dimensional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
     let result_array = [];
-    if(matrix.length) result_array = matrix.map(function(value,index,array){
-        if(index/2) return value.reverse();
-        else return value;
-    })
-  return result_array;
+    if (matrix.length) {
+        let sort_array = matrix.map(function (value, index, array) {
+            if (index % 2) return value.reverse();
+            else return value;
+        })
+        result_array = sort_array.reduce((array,item)=>array.concat(item))
+
+    }
+    return result_array;
 }
