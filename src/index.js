9b54e0c1b5e7b80e7bb132969dc781c0ce9d906a
Nadin21 <nadin21dir@gmail.com>
2023-08-19 16:25:08 +0300

    fix: checking for emptiness

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    let result_array = matrix.map(function(value,index,array){
+    let result_array = [];
+    if(matrix.length) result_array = matrix.map(function(value,index,array){
         if(index/2) return value.reverse();
         else return value;
     })
